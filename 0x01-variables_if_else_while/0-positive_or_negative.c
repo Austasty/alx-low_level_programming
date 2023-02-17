758cb65c3598d5c32c9592e1ea8ca1a821512857
Austasty <obuesiogonna2@gmail>
2023-02-17 14:54:59 -0800

    error  tackled

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,10 +17,10 @@ srand(time(0));
 if (n > 0)
 {
 	printf("%d is positive\n", n);
-} else if (n == 0)
+}if (n == 0)
 {
 	printf("%d is zero\n", n);
 } else
 	printf("%d is negative\n", n);
-	return (0);
+return (0);
 }
