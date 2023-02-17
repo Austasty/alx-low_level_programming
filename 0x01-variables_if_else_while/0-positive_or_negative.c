003a82e78ad8603d51e49b794c7f0b2ae8096c28
Austasty <obuesiogonna2@gmail>
2023-02-17 13:58:33 -0800

    random number search

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,14 +16,11 @@ srand(time(0));
 	/* your code goes there */
 if (n > 0)
 {
-	printf0("%d is positive\n", n);
-if (n == 0)
+	printf("%d is positive\n", n);
+} else if (n == 0)
 {
 	printf("%d is zero\n", n);
-}
-else
-{
+} else
 	printf("%d is negative\n, n);
-}
 	return (0);
 }
