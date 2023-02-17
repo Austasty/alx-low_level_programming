bea5416eab865b64fd72b987e6c033f1b3224e11
Austasty <obuesiogonna2@gmail>
2023-02-17 15:08:13 -0800

    checked errors and corrected

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,10 +17,11 @@ srand(time(0));
 if (n > 0)
 {
 	printf("%d is positive\n", n);
-}if (n == 0)
+} else if (n == 0)
 {
 	printf("%d is zero\n", n);
-} else
+} else{
 	printf("%d is negative\n", n);
-return (0);
+}
+	return (0);
 }
