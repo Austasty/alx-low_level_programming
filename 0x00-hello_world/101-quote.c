0c3bc3135f81fcf5aacde690408a3bf8d6977bcf
Austasty <obuesiogonna2@gmail>
2023-02-17 10:08:47 -0800

    printing to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always  (Success)
- */
+* main - Entry point
+*
+* Return: Always  (Success)
+*/
 int main(void)
 {
-	write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",59);	
-       	return (1);
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
