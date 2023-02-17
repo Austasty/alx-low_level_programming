075af8628f555196cd5f675e1120bf5c407e1ac7
Austasty <obuesiogonna2@gmail>
2023-02-17 07:50:42 -0800

    prints a string

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * prints exactly with proper grammar, but the outcome is a piece of art
- * Return 0
+ * main - printing a string followedd by a new line
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
