34a9bfc411b9bfeca426f50503a6e74f631fd393
Austasty <obuesiogonna2@gmail>
2023-02-17 17:18:23 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,5 +22,7 @@ srand(time(0));
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	else if (lastDigit == 0)
 	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	else
+	printf("less than 6 and not 0\n");
 return (0);
 }
