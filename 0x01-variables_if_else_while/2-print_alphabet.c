36a5eec054721dae99d5b49774677499bc159992
Austasty <obuesiogonna2@gmail>
2023-02-18 08:23:55 -0800

    rinting alphabet in lower  case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+*main - prints random number in  variable n is positive or negative
+*Return: 0
+*/
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
