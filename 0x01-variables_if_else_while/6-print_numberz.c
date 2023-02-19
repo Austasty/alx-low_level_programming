9b2db9b888f197e8a237c4a942ef791e070d1310
Austasty <obuesiogonna2@gmail>
2023-02-19 08:58:29 -0800

    printing using putchar in base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * A program that prints the numbers in base 10 starting from 0 using putchar
+ * Return: 0 always (Success)
+ */
+int main(void)
+{
+	int num2;
+
+	for (num2 = 0; num2 <= 9; num2++)
+		putchar(num2 +'0');
+	putchar('\n');
+	return (0);
+}
