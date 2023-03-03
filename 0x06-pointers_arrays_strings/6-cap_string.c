d49cb83698205e9bd6c8096bedeb599d8859ec00
Austasty <obuesiogonna2@gmail>
2023-03-02 18:45:08 -0800

    printing capital

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,41 @@
 #include "main.h"
+
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
- *
- * Return: A pointer to the changed string.
+ * _islower - checks if a character is lowercase
+ * @c: character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
-
-char *cap_string(char *str)
+static int _islower(int c)
 {
-	int index = 0;
+	return (c >= 'a' && c <= 'z');
+}
 
-	while (str[index])
-	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
+/**
+ * _isseparator - checks if a character is a separator
+ * @c: character to check
+ * Return: 1 if c is a separator, 0 otherwise
+ */
+static int _isseparator(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n' ||
+			c == ',' || c == ';' || c == '.' ||
+			c == '!' || c == '?' || c == '"' ||
+			c == '(' || c == ')' || c == '{' ||
+			c == '}');
+}
 
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-		    str[index] -= 32;
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to capitalize
+ * Return: pointer to string s
+ */
+char *cap_string(char *s)
+{
+	int i;
 
-		index++;
-	}
+	for (i = 0; s[i]; ++i)
+		if (i == 0 || _isseparator(s[i - 1]))
+			(_islower(s[i])) ? s[i] -= 32 : 0;
 
-	return (str);
+	return (s);
 }
