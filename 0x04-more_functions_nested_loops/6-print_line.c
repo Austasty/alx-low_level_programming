639464d49fe6538683c97f535261fa33140a0121
Austasty <obuesiogonna2@gmail>
2023-03-04 06:31:47 -0800

    drawing straigth line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * To draws a straight line
+ * @n: Integer variable
+ * Return: int
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
