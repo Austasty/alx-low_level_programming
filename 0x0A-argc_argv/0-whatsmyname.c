333ad010d54afa072768fea73f2d70cf9022b17b
Austasty <obuesiogonna2@gmail>
2023-03-10 16:40:50 -0800

    printing newname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printing the name of a string
+ * @argc: the argument value count
+ * @argv: the array value
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
