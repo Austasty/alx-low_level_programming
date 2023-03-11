66c94604ea3ccaa0bc50ac77a19a53d6c7ad75c2
Austasty <obuesiogonna2@gmail>
2023-03-10 17:03:02 -0800

    updated

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - printing word count
+ *@argc: arguments to count
+ *@argv: value of element
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argv;
+	printf("%d", argc - 1);
+	return (0);
+}
