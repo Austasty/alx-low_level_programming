7af82d0bc81439adc857220bf3d59e92024ccc82
Austasty <obuesiogonna2@gmail>
2023-03-20 12:42:41 -0700

    creating a structure to store dog multiple information

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+this is all about structures in c.
+ A strustioon s a user define data type that stores any valid c data type 
+unlike an array that takes many input but of same data type. 
+A c struct or structure make works easier especially in a management system
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog- the user define date type
+ * @name: character pointer to name member
+ * @age: second member
+ * @owner: third member point in struct
+ * Return 0 (success);
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
