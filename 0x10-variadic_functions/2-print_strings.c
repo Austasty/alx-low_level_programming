f396f556cde2cf46e1a3abcbd6a0d778fc618290
Austasty <obuesiogonna2@gmail>
2023-03-24 02:59:48 -0700

    characters

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,7 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", str);
 		}
-		if (separator != NULL && i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
