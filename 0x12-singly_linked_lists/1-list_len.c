108a16465580ca1a58885a1c35527caf81f0cddd
Austasty <obuesiogonna2@gmail>
2023-03-31 08:22:08 -0700

    counting the nodes

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,13 +9,15 @@
  * Return: count;
  */
 
-size_t list_len(const list_t *h){
+size_t list_len(const list_t *h)
+{
 	size_t count = 0;
-	
 	const list_t *current = h;
-	while (current != NULL){
+
+	while (current != NULL)
+	{
 		count++;
 		current = current->next;
 	}
-	return count;
+	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -10,11 +9,12 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
