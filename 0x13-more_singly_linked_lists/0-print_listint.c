686b2f71021fd12b5339825cc5dfcca42c80a37d
Austasty <obuesiogonna2@gmail>
2023-04-05 13:17:12 -0700

    prints a value of n

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h=h->next;
+		count++;
+	}
+	return (count);
+}
