1b985d36e45f5a4ff74fe914a5c70aac89b5f051
Austasty <obuesiogonna2@gmail>
2023-04-05 13:55:33 -0700

    counting elements and returning the count

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	while(h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
