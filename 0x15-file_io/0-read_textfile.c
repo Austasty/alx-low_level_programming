14dce716e37357f1555bfd8e99aef76a6e1efd07
Austasty <obuesiogonna2@gmail>
2023-04-08 08:37:12 -0700

    creating a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * read_textfile - Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w - actual number of bytes read and printed
+ * 	0 when function fails or filename is NULL.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t w;
+	ssize_t r;
+	FILE *ptr;
+
+	ptr = fopen(filename, "r");
+	if(ptr == NULL)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	r = read(fileno(ptr), buffer, letters);
+	w = write(STDOUT_FILENO, buffer,r);
+	free(buffer);
+	fclose(ptr);
+	if(w != r)
+		return (0);
+	return (w);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ * 	Otherwise - 1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int ptr,w,i=0;
+
+	if(filename == NULL)
+		return (-1);
+	if(text_content != NULL){
+		for(i = 0;text_content[i];i++)
+	}
+	ptr = open(filename, O_CREAT|O_RDWR|O_TRUNC, 0600);
+	w = write(ptr, text_content, i);
+	if(ptr ==-1 || w == -1)
+		return(-1);
+	close(ptr);
+	return (1);
+}
