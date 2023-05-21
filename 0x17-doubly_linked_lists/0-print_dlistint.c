f6db6648cc4455f9fbf5e30e89843b3326edeaae
Austasty <obuesiogonna2@gmail.com>
2023-05-21 07:45:10 -0700

    printing the element of a node

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: The head pointer.
+ *
+ * Return: The element of a distin_t  in the list.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
+}
