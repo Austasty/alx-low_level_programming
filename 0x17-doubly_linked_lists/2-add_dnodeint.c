280f84bc1eaba4644fa1611fa282ae0ed2faecdb
Austasty <obuesiogonna2@gmail.com>
2023-05-21 08:35:50 -0700

    adds a newnode at the begining of a distinct_lisxst

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Adds new node at the beginning of a dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ * @n: The integer for the new node.
+ * Return: If the function fails - NULL.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+	return (new);
+}
