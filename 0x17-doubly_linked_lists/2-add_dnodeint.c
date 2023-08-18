d350e919338b099d760a5dbfb91ef997f29c24a0
Austasty <obuesiogonna2@gmail.com>
2023-08-18 01:29:20 -0700

    fix a bug

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - Adds new node at the beginning of a dlistint_t list.
+ * add_dnodeint - Adds new node at the beginning of a list.
  * @head: A pointer to the head of the dlistint_t list.
  * @n: The integer for the new node.
- * Return: If the function fails - NULL.
+ * Return: NULL If the function fails
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
- * @head: A pointer to the head.
- * @n: The integer for the new node.
- * Return: If the function fails - NULL.
+ * add_dnodeint_end - Adds a new node at the end of a doubly linked list.
+ * @head: A pointer to the head
+ * @n: The integer for the new node
+ * Return: NULL If the function fails
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
