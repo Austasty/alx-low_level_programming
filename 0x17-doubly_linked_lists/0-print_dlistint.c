ffe1f4c5e6bd63e78138605047520c6b86b161b4
Austasty <obuesiogonna2@gmail.com>
2023-08-18 01:24:54 -0700

    fix a bug

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,8 +2,8 @@
 
 /**
  * print_dlistint - Prints all the elements of a dlistint_t list.
- * @h: The head pointer.
- * Return: The element of a distin_t  in the list.
+ * @h: pointer to the head node
+ * Return: The element of a distin_t node in the list.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,8 +2,8 @@
 
 /**
  * dlistint_len - Counts the number of elements in a linked dlistint_t list.
- * @h: The head of the dlistint_t list.
- * Return: The number of elements in a linked  dlistint_t list.
+ * @h: The head of the dlistint_t list
+ * Return: The number of elements in a linked list.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
