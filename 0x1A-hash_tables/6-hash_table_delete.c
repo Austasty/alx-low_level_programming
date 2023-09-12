cecdaff9da272256ae8ce20ed2cd4f65a6d2cd48
Austasty <obuesiogonna2@gmail.com>
2023-09-12 15:05:48 +0000

    added

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes a hash table element
+ * hash_table_delete - Deletes a hash table
  * @ht: A pointer to the hash table.
  */
 void hash_table_delete(hash_table_t *ht)
